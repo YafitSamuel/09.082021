1b0c515a4c31deea71c4b8d632d834565c556395
YafitSamuel <gyafit642@gmail.com>
2021-08-10 08:31:11 +0300

    commit

---
+++ function.js
@@ -0,0 +1,84 @@
+// -----------משימת  בית 09.08.2021------------------
+
+
+// --------א-------
+
+// const Numbers = [1, 2, 3, 4];
+// function myFunction(Numbers) {
+//     var x = Numbers.reverse();
+   
+//    return x;
+// }
+//  var y= myFunction(Numbers)
+// console.log(y)
+
+// ---------------2-----------------
+
+// var Numbers = [1,2 ,3, 4,5 ,6 , 7 , 8 ,9,10];
+// function numArr(Numbers){
+//     var num="";
+//     for(var i=0; i<Numbers.length;i++){
+//         num+=Numbers[i]
+//         if(i==2){
+//             num+="-"
+        
+//         }else if(i==5){
+//             num+="-"
+//         }
+//     }
+//        return num    
+//     }
+// console.log(numArr(Numbers));
+
+
+// -----------------3------------------
+    
+
+
+
+
+
+
+
+// ----------------------4------------------ 
+
+// var a=1;
+// function x(params) {
+//     for(var i=1; i<=params;i++){
+//         a=a*i
+//     }
+//     return a
+// }
+// console.log(x(4));
+
+// -----------------------5--------------------
+
+// function x(num1, num2) {
+//     sum= num1+ num2
+//     return sum
+// }
+// console.log(x(4, 4))
+
+
+
+// function x(num1, num2) {
+//     sum= num1-num2
+//     return sum
+// }
+// console.log(x(7, 4))
+
+
+// function x(num1, num2) {
+//     sum= num1* num2
+//     return sum
+// }
+// console.log(x(3, 4))
+
+
+
+// function x(num1, num2) {
+//     sum= num1/ num2
+//     return sum
+// }
+// console.log(x(15, 3))
+
